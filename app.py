d91b38885e064e53d948b2fa9f4ad996780c37f4
Tung Nguyen <tongueroo@gmail.com>
2025-04-02 15:40:23 +0000

    env var PORT

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -25,4 +26,5 @@ def echo():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
